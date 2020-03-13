f944641c24ab612926e11edf25e6c0cfcd52f85f
ramos <rareramos@outlook.ph>
2020-03-13 21:26:29 +0200

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { createWorkerAddon } from '@watchedcom/sdk';
-import { directoryHandler, itemHandler, sourceHandler } from './handlers';
+import { createWorkerAddon, WorkerHandlers } from '@watchedcom/sdk';
+import twitch from './twitch';
 
 export const twitchAddon = createWorkerAddon({
   id: 'twitch',
@@ -11,19 +11,34 @@ export const twitchAddon = createWorkerAddon({
     displayName: true,
   },
   defaultDirectoryFeatures: {
-    search: { enabled: true },
+    //search: { enabled: true },
   },
   dashboards: [
     {
-      id: 'games',
-      name: 'Twitch Games',
-    },
-    {
       id: '',
-      name: 'Top Twitch Channels',
+      name: 'Twitch Games',
     },
   ],
 });
+
+// input {id, search, filter, page}
+const directoryHandler: WorkerHandlers['directory'] = async (input: any, ctx) => {
+  if (input.id) {
+    return await twitch.getChannels(input);
+  } else if (input.search) {
+    return await twitch.searchChannels(input);
+  } else {
+    return await twitch.getGames(input);
+  }
+};
+
+const itemHandler: WorkerHandlers['item'] = async (input: any, ctx) => {
+  return await twitch.getChannel(input);
+};
+
+const sourceHandler: WorkerHandlers['source'] = async (input, ctx) => {
+  return [];
+};
 
 twitchAddon.registerActionHandler('directory', directoryHandler);
 
